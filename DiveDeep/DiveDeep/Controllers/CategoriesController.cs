2ea4542a32e10ee2814acf3452399d7a2882de72
Krause1499 <mads.krag99@gmail.com>
2025-08-29 10:39:03 +0200

    Small details

---
+++ DiveDeep/DiveDeep/Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiveDeep.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult BCD()
+        {
+            return View();
+        }
+        public IActionResult Dykkerdragter()
+        {
+            return View();
+        }
+        public IActionResult Tanke()
+        {
+            return View();
+        }
+        public IActionResult Regulatorsæt()
+        {
+            return View();
+        }
+        public IActionResult MaskerSnorkler()
+        {
+            return View();
+        }
+        public IActionResult Finner()
+        {
+            return View();
+        }
+    }
+}

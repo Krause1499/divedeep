dc6b374bc4136626d349bd0e4d67b517c60cc1ee
Krause1499 <mads.krag99@gmail.com>
2025-10-05 05:08:43 +0200

    Start

---
+++ DiveDeep/DiveDeep/Data/DiveDeepContext.cs
@@ -0,0 +1,21 @@
+﻿using DiveDeep.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiveDeep.Data
+{
+    public class DiveDeepContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var m = modelBuilder.Entity<Product>();
+            m.OwnsOne(p => p.DivingSuit);
+            m.OwnsOne(p => p.BCD);
+            m.OwnsOne(p => p.Fins);
+            m.OwnsOne(p => p.OxygenTank);
+            m.OwnsOne(p => p.Regulator);
+            m.OwnsOne(p => p.MaskSnorkel);
+        }
+    }
+}
